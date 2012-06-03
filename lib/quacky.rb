5986d207d2d7f97942c2f7490903e8beaa714375
Matt Parker <moonmaster9000@gmail.com>
2012-06-02 20:49:09 -0400

    message expectations

--- lib/quacky.rb
+++ lib/quacky.rb
@@ -1,9 +1,66 @@
 module Quacky
   extend self
+  class Double; end
+
+  class NoMethodError < RuntimeError; end
+
+  class MessageExpectation
+    def initialize(method)
+      @method = method
+    end
+
+    def with(*args)
+      @expected_args = args
+      call_through *args
+    end
+
+    def and_return(value=nil, &block)
+      @return_value = value
+      @return_block = block
+    end
+
+    def call(*args)
+      @called_args = args
+      call_through *args
+      return_value
+    end
+
+    def satisfied?
+      if expected_args
+        called_args == expected_args
+      else
+        !!called_args
+      end
+    end
+
+    private
+    attr_reader :called_args, :expected_args
+
+    def return_value
+      return @return_value if @return_value
+      @return_block.call if @return_block
+    end
+
+    def call_through *args
+      begin
+        @method.call *args
+      rescue ArgumentError => e
+        raise Quacky::MethodSignatureMismatch, e.message
+      end
+    end
+  end
+
+  module Expectations
+    def stub method_name
+      raise Quacky::NoMethodError unless respond_to? method_name
+      MessageExpectation.new public_method(method_name)
+    end
+  end
 
   def double duck_type
-    Object.new.tap do |object|
+    Double.new.tap do |object|
       object.extend duck_type
+      object.extend Quacky::Expectations
     end
   end
 
@@ -26,8 +83,8 @@ module Quacky
     class_modules    = options.fetch :class
     instance_modules = options.fetch :instance
 
-    class_modules    = [class_modules] unless class_modules.kind_of? Array
-    instance_modules = [instance_modules] unless instance_modules.kind_of? Array
+    class_modules    = [class_modules] unless class_modules.respond_to? :each
+    instance_modules = [instance_modules] unless instance_modules.respond_to? :each
 
     [class_modules, instance_modules]
   end
--- spec/lib/quacky_spec.rb
+++ spec/lib/quacky_spec.rb
@@ -1,39 +1,24 @@
 require_relative '../../lib/quacky'
 
+module Duck
+  def duck arg
+  end
+end
+
 describe Quacky do
   describe "#double" do
-    let(:duck) do Mod = Module.new end
-    let(:eigenclass) { class << Quacky.double(duck); self; end }
+    let(:eigenclass) { class << Quacky.double(Duck); self; end }
 
     subject { eigenclass }
 
-    its(:included_modules) { should include duck }
+    its(:included_modules) { should include Duck }
   end
 
   describe "#class_double" do
-    context "single class and instance module" do
-      let(:class_duck)    { Module.new }
-      let(:instance_duck) { Module.new }
-      let(:class_double)  { Quacky.class_double class: class_duck, instance: instance_duck }
-      let(:class_eigenclass) { class << class_double; self; end }
+    let(:class_double)  { Quacky.class_double class: class_ducks, instance: instance_ducks }
+    let(:class_eigenclass) { class << class_double; self; end }
 
-      context "instance methods" do
-        subject { class_double }
-        its(:ancestors) { should include *instance_duck }
-      end
-
-      context "class methods" do
-        subject { class_eigenclass }
-        its(:included_modules) { should include *class_duck }
-      end
-    end
-
-    context "multiple class and instance modules" do
-      let(:class_ducks)      { [ Module.new, Module.new ] }
-      let(:instance_ducks)   { [ Module.new, Module.new ] }
-      let(:class_double)     { Quacky.class_double class: class_ducks, instance: instance_ducks }
-      let(:class_eigenclass) { class << class_double; self; end }
-
+    shared_examples_for "quacky class double" do
       context "instance methods" do
         subject { class_double }
         its(:ancestors) { should include *instance_ducks }
@@ -42,6 +27,102 @@ describe Quacky do
       context "class methods" do
         subject { class_eigenclass }
         its(:included_modules) { should include *class_ducks }
+      end
+    end
+
+    context "single class and instance module" do
+      let(:class_ducks)    { Module.new }
+      let(:instance_ducks) { Module.new }
+
+      it_behaves_like "quacky class double"
+    end
+
+    context "multiple class and instance modules" do
+      let(:class_ducks)      { [ Module.new, Module.new ] }
+      let(:instance_ducks)   { [ Module.new, Module.new ] }
+
+      it_behaves_like "quacky class double"
+    end
+  end
+
+  describe Quacky::Double do
+    describe ".stub" do
+      let(:q_double) { Quacky.double Duck }
+
+      it "should raise an exception if the method does not already exist on the double" do
+        expect { q_double.stub("random_method_#{rand 1000000}") }.to raise_exception Quacky::NoMethodError
+      end
+
+      it "should initialize and return a new Quacky::MessageExpectation otherwise" do
+        Quacky::MessageExpectation.should_receive(:new).with q_double.public_method(:duck)
+        q_double.stub(:duck)
+      end
+    end
+  end
+end
+
+module Quacky
+  describe MessageExpectation do
+    let(:object) do
+      Class.new do
+        include Duck
+      end.new
+    end
+
+    let(:q_expectation) { Quacky::MessageExpectation.new(object.public_method(:duck)) }
+    
+    describe "#with" do
+      it "should raise an exception if the original method's signature mismatches" do
+        expect { q_expectation.with 1,2,3 }.to raise_exception Quacky::MethodSignatureMismatch
+      end
+    end
+
+    describe "#and_return" do
+      context "static value" do
+        it "should return the static value when called" do
+          return_value = double :return_value
+          q_expectation.and_return return_value
+          q_expectation.call(double :argument).should == return_value
+        end
+      end
+
+      context "block return value" do
+        it "should return the value returned by the block" do
+          return_value = double :return_value
+          q_expectation.and_return { return_value }
+          q_expectation.call(double :argument).should == return_value
+        end
+      end
+    end
+
+    describe "#call" do
+      subject { q_expectation }
+
+      context "called with invalid arguments" do
+        it "should raise an exception" do
+          expect{ q_expectation.call(1,2,3)}.to raise_exception Quacky::MethodSignatureMismatch
+        end
+      end
+
+      context "no with expectation" do
+        before { q_expectation.call double(:argument) }
+        it { should be_satisfied }
+      end
+
+      context "with expectation" do
+        let(:argument) { double :argument }
+
+        before { q_expectation.with argument }
+
+        context "called with matching argument" do
+          before { q_expectation.call argument }
+          it { should be_satisfied }
+        end
+
+        context "called with non-matching argument" do
+          before { q_expectation.call rand }
+          it { should_not be_satisfied }
+        end
       end
     end
   end
